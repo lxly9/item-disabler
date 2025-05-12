1732ae57b345fdb66a919da8db888e73d60f2bf6
Lily <lilyschlossnagel@icloud.com>
2025-05-12 14:29:12 +0200

    Initial commit

---
+++ src/main/java/com/item_disabler/mixin/ItemMixin.java
@@ -0,0 +1,14 @@
+package com.item_disabler.mixin;
+
+import net.minecraft.item.Item;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Item.class)
+public class ItemMixin {
+	@Inject(at = @At("HEAD"), method = "loadWorld")
+	private void init(CallbackInfo info) {
+	}
+}
